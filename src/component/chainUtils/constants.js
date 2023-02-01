ecf786362319ca3685c332704e0f56c87ba199e3
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2023-02-01 20:55:24 +0530

    aded

---
+++ src/component/chainUtils/constants.js
@@ -0,0 +1,34 @@
+import abi from "./abi.json";
+import balance from "./balance.json";
+import tokenAbi from "./token.json";
+
+
+//fomo3dlong game 0x51A5271Ec514c3065d9de2D8E95051989f7D53AB
+//fomo soon game 0x4e8ecF79AdE5e2C49B9e30D795517A81e0Bf00B8
+//fomo short testnet 0xd7987938613B467344155b4bbD9EeF27Ef483138
+//0xf4c644637C7e08285061ef0DF3739b414A8A2F97
+//main 0x422e58Fa0c33766ec2659d8534e85089DBDeC15c
+export const gameContract = "0x8C6e7ecC13C1e978D5ee8B5EB9d17b993149AbA1";
+
+
+
+//fomo3dlong balance 0xc7029Ed9EBa97A096e72607f4340c34049C7AF48
+//fomosoon balance  0xC0c001140319C5f114F8467295b1F22F86929Ad0
+//fomoshort testnet  0xfa24a84f5d224E500b7625357D862FB7115eE426
+export const balanceContract = "0x728b662D78abB9092c1Bceaa60F89676e8857389";
+
+
+
+export const tokenContract = "0x6c73a325b9fA2D17296f8008805bb3ddd2F4484d";
+
+
+
+export const gameABI = abi;
+export const balanceABI = balance;
+export const ercABI = tokenAbi;
+
+
+
+export const chainID = 5;
+
+
